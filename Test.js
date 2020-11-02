e4ece4c838e07f746b0e88324c268ddeced1409c
JuneKim0712 <Junwoo.Kim0712@gmail.com>
2020-11-01 23:09:51 -0800

    init Canculator

--- Test.js
+++ Test.js
@@ -1,11 +1,39 @@
-const f = document.getElementById("f")
+const input = document.getElementById("input")
+const answer = document.getElementById("answer")
 
-function update(e) {
-    if (e.key == 'Enter') {
-        f.innerHTML = ''
+function GetAnswer() {
+    let check = CheckAnswer()
+    if (check == true) {
+        answer.innerHTML = `${input.value} = ` + String(eval(input.value))
+        input.value = ''
     } else {
-        f.innerHTML += e.key
+        answer.innerHTML = 'Error'
+        input.value = ''
     }
 }
 
-window.addEventListener('keydown', e => update(e))
+function CheckAnswer() {
+    const checklist = [' ', '0', '1', '2', '3', '4', 
+    '5', '6', '7', '8', '9', '/', '.', '%', 
+    '+', '[', ']', '(', ')', '*']
+    let leninput = input.value.length
+    let index = 0
+    while (leninput > index) {
+        let char = input.value[index]
+        if (checklist.includes(char)) {
+            index += 1
+            continue
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+window.addEventListener('keydown', e => {
+    if (e.key == 'Enter' && input.value != '') {
+            GetAnswer()
+        } else {
+        return
+    }
+})
