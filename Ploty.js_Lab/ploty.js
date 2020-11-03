be2300009caa3e7a0135a5bb456c5420f19e71b2
JuneKim0712 <Junwoo.Kim0712@gmail.com>
2020-11-02 19:15:57 -0800

    Made graph ratio into 1:1

--- Ploty.js_Lab/ploty.js
+++ Ploty.js_Lab/ploty.js
@@ -4,19 +4,16 @@ function fx() {
         y: [0],
         mode: 'lines'
     };    
-    for (i=10;i > plot_1.y[plot_1.x.length-1];) {
+    for (i=100000;i > plot_1.y[plot_1.x.length-1];) {
     plot_1.x.push(plot_1.x[plot_1.x.length-1] + 0.1);
     plot_1.y.push(plot_1.x[plot_1.x.length-1] ** 2);
     };
-    let fixed = {
-        autosize: false,
-        width: 0,
-        height: 0,
-        x: 0,
-        y: 0,
-        mode: "lines"
-    };
-    return [plot_1, fixed]
+    return [plot_1]
 };
+var layout = {
+    yaxis: {
+      scaleanchor: "x"
+    }
+}
 
-Plotly.newPlot('plot', fx());
+Plotly.newPlot('plot', fx(), layout);
